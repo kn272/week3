8c02e5613cd1fe6e725b7be33b6f0136fe2aa9de
kishore kumar reddy nerkundram gajendran <kn272@afsconnect1.njit.edu>
2017-09-26 23:57:30 -0400

    added cons and dest

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
-echo "hello world"
+
+  $obj = new main();
+
+  class main {
+    public function __construct() {
+       $text = "program started"
+    }
+    public function __destruct() {
+       $text = "program ended"
+    }
 ?>

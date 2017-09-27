98a0276c3a3e2282bd71f02e57dc12f81c21bd23
kishore kumar reddy nerkundram gajendran <kn272@afsconnect1.njit.edu>
2017-09-27 00:45:09 -0400

    basic structure-with bugs

--- index.php
+++ index.php
@@ -3,10 +3,39 @@
   $obj = new main();
 
   class main {
+   
     public function __construct() {
-       $text = "program started"
+       $text = "program  started<br>";
+       echo "$text";
+       $text = "<h1>String Function Demonstration-PRINT</h1><br>";
+       stringFunctions::printhis($text);
+       $text = "Sample Text<br>";
+       stringFunctions::printhis($text);
+       $text = "<hr><br>";
+       stringFunctions::printhis($text);
+       $text = "<h1>Array Function Demonstration-ARRAY</h1><br>";
+       stringFunctions::printhis($text);
+       $myArray = array(1,2,3,4,5);
+       arrayFunctions::printArray($myArray);
+       $text = "<hr><br>";
+       stringFunctions::printhis($text);
     }
+
     public function __destruct() {
-       $text = "program ended"
-    }
+       $text = "program ended<br>";
+       echo "$text";
+    } 
+  }
+  class stringFunctions {
+     static public function printThis($text) {
+        print($text);
+     }
+  }
+
+  class arrayFunctions {
+     static public function printArray($myArray) {
+        array($myArray);
+     }
+  }
+
 ?>
